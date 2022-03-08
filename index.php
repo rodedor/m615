9c050a453b56b93cbbd0c77637e4dc1567a5de72
rodedor <101195905+rodedor@users.noreply.github.com>
2022-03-08 17:32:47 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+ <meta charset="UTF-8">
+ <title>Вася</title>
+ <meta name="description" content="Данные по работе">
+</head>
+
+<body>
+<h1>Вася</h1>
+<h1>Табель</h1>
+<table>
+    <tr>
+        <th>
+            Дата
+        </th>
+        <th>
+            Часы
+        </th>
+        <th>
+            Примечание
+        </th>
+    </tr>
+    <tr>
+        <td>
+            01.01.2000
+        </td>
+        <td>
+            8
+        </td>
+        <td>
+            
+        </td>
+        <tr>
+        <td>
+            02.01.2000
+        </td>
+        <td>
+            8
+        </td>
+        <td>
+            
+        </td>
+        <tr>
+        <td>
+            03.01.2000
+        </td>
+        <td>
+            8
+        </td>
+        <td>
+            По факту 6 часов
+        </td>
+    </tr>
+</table>
+
+<br>
+Оплата за час: 125р
+<br>
+Оплата за смену: 1000р
+
+<h1>Выдано</h1>
+<table>
+    <tr>
+        <th>
+            Дата
+        </th>
+        <th>
+            Сумма
+        </th>
+        <th>
+            Примечание
+        </th>
+    </tr>
+    <tr>
+        <td>
+            01.01.2000
+        </td>
+        <td>
+            1 000
+        </td>
+        <td>
+            
+        </td>
+        <tr>
+        <td>
+            02.01.2000
+        </td>
+        <td>
+            900
+        </td>
+        <td>
+            Мелкими
+        </td>
+        <tr>
+        <td>
+            03.01.2000
+        </td>
+        <td>
+            10 000
+        </td>
+        <td>
+            Аванс
+        </td>
+    </tr>
+</table>
+
+Остаток: 9 999р.
+
+<?php
+echo " php!";
+?>
+</body>
+</html>
